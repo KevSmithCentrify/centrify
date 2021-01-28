e4e63a52cbae082fdecdc897e877af3e80105def
Kev Smith <kev.smith@centrify.com>
2021-01-28 14:24:29 +0000

    Update postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -292,11 +292,11 @@ fi
 
 # Install pre-prod client
 
-if ! curl --silent -o /tmp/cclient.rpm https://edge.clouddev.centrify.com/clidownload/station/CentrifyCC-rhel6.x86_64.rpm >> $centrifycc_deploy_dir/deploy.log 2>&1;
+if ! curl --silent -o /tmp/CentrifyCC-rhel6.x86_642.rpm https://edge.clouddev.centrify.com/clidownload/station/CentrifyCC-rhel6.x86_64.rpm >> $centrifycc_deploy_dir/deploy.log 2>&1;
 then
 echo 'postbuild: failed to curl https://edge.clouddev.centrify.com/clidownload/station/CentrifyCC-rhel6.x86_64.rpm' >> $centrifycc_deploy_dir/deploy.log 2>&1
 else
-    if ! rpm -Uv /tmp/cclient.rpm >> $centrifycc_deploy_dir/deploy.log 2>&1;
+    if ! rpm -Uv /tmp/CentrifyCC-rhel6.x86_642.rpm >> $centrifycc_deploy_dir/deploy.log 2>&1;
     then
         echo 'postbuild: rpm upgrade on cclient failed' >> $centrifycc_deploy_dir/deploy.log 2>&1;
     else
